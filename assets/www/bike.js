2ad6ae25579b3519a471313ac60de03e28ac0bbc
Matthew Willse <mwillse@gmail.com>
2012-05-01 22:58:57 -0400

    added username form. distance calculationwho knows what else.

--- assets/www/bike.js
+++ assets/www/bike.js
@@ -3,9 +3,10 @@
  */
 
 var write_to_carto = false;
-var write_local_db = false;
+var write_local_db = true;
 var dropadd_local_db = true; // clear local DB
 var reset_rideNum = false; // clear localStorage
+var reset_distLifetime = false; // clear localStorage
 var use_dummy_data = true; // true for off-phone browser dev
 
 
@@ -19,8 +20,10 @@ var cartoKey = "d1003f790f91855f9a72363ac887e14010974332";
 
 // setup ride vars
 var gpsInterval = 3000; // milliseconds
-var startLat, startLong, endLat, endLong; // used for rough distance
+var startLat, startLong,prevLat, prevLong; // used for rough distance
+var distance, distLifetime, distRide = 0;
 var userID = 17;
+var username;
 var rideID; // localStorage.rideNum
 var counter=0;
 var startTime, endTime;
@@ -28,13 +31,30 @@ var timer;
 var timer_is_on=0;
 
 
+
+function initBike() {
+    // check for username
+    if ( !localStorage.getItem('username') ) { 
+        //window.location.href = "settings.html"
+        document.getElementById('settings').style.display = "block";
+        document.getElementById('stats').style.display = "none";
+    }
+    else {
+        username = localStorage.getItem('username');
+        console.log("username: " + username); 
+    }
+    
+    lifetimeDistance();
+     
+}
+
 // set up local storage
 if (reset_rideNum) { 
-    console.log("clear localStorage"); 
+    console.log("clear ride number"); 
     localStorage.removeItem('rideNum'); 
 }
 if ( !localStorage.getItem('rideNum') ) { 
-    console.log("init localStorage"); 
+    console.log("init ride number"); 
     localStorage.setItem('rideNum',0);
 }
 localStorage.setItem('userID',userID);
@@ -47,14 +67,21 @@ if (dropadd_local_db) { dbDrop(); }
 else { init_db(); }
 
 
-
-function initmap() {
-    
-    var mapurl = "https://ideapublic.cartodb.com/tables/rides/embed_map?sql=SELECT%20*%20FROM%20rides%20where%20user_id%3D"+userID;
-    
-    document.getElementById('mapframe').src = mapurl;
-    
+// clear lifetime distance
+if (reset_distLifetime) { 
+    console.log("clear lifetime distance"); 
+    localStorage.removeItem('distLifetime'); 
 }
+// check total distance
+function lifetimeDistance() {
+    if ( !localStorage.getItem('distLifetime') ) { 
+        console.log("init lifetime distance"); 
+        localStorage.setItem('distLifetime',0);
+    }
+    distLifetime = Number(localStorage.distLifetime); // convert, stored as string.
+    document.getElementById('dist-Lifetime').innerHTML = displayDistance(distLifetime);    
+}
+
 
 /******************************* 
  * User Actions
@@ -68,6 +95,8 @@ function iotbike() {
         toggleUI();
 
         rideID = rideID + 1;
+        document.getElementById('distance').innerHTML = "0.00 km";
+        
 
         startTime = new Date();
         startTimer();
@@ -98,12 +127,17 @@ function iotOff() {
     var startMinutes = startTime.getMinutes();
     var endMinutes = endTime.getMinutes();
     
-    console.log("rideID " + rideID +" complete with " + counter +" points in " + elapsed + " seconds.");
+    distLifetime = distLifetime + distRide;
+    localStorage.setItem('distLifetime',distLifetime);
+    lifetimeDistance();
+    
+    console.log("rideID " + rideID +" complete: " + distRide +" meters & " + counter +" points in " + elapsed + " seconds.");
+    console.log("lifetime Distance " + distLifetime );
     
     cartodbLine(rideID);
     // could clear local DB if cartodbLine returns ok
 
-    alert("Ride #" + rideID + " is complete with " + counter + " points.");
+    alert("Ride #" + rideID + " is complete with " + distRide + " meters.");
     counter = 0;
     //rideCheck();
 }
@@ -131,8 +165,6 @@ function toggleUI() {
     else {
         startbutton.style.display = "none";
         stopbutton.style.display = "block";        
-        data.style.display = "block";
-        hardware.style.display = "none";    
         maplink.style.display = "none";
         clock.style.color = "#fff";
     }
@@ -143,9 +175,15 @@ function toggleUI() {
 // could be run on CartoDB xmlHttp.responseText
 function feedback() {    
     document.getElementById('ride-number').innerHTML = "Ride #" + rideID;
-    document.getElementById('background').innerHTML = "Data transmission log. User: " + userID + ". Ride: " + rideID + ". ";
+    document.getElementById('opendata').innerHTML = "Data transmission log. User: " + userID + ". Ride: " + rideID + ". ";
     
 }
+
+function initmap() {
+    var mapurl = "https://ideapublic.cartodb.com/tables/rides/embed_map?sql=SELECT%20*%20FROM%20rides%20where%20user_id%3D"+userID;    
+    document.getElementById('mapframe').src = mapurl;
+}
+
 
 /******************************* 
  * User & Ride Data
@@ -153,34 +191,41 @@ function feedback() {
 
 
 // smartly random for fakeLocation()
-    var randA = Math.random()/100;
-    var randB = Math.random()/100;
-    var randX = Math.round( Math.random() * 10 ) / 10 ;
+    var randX = Math.round( Math.random() * 10 ) / 10 ; // start location
     var randY = Math.round( Math.random() * 10 ) / 10 ;
+    var randA = Math.random()/5000; // speed
+    var randB = Math.random()/5000;
 
 function fakeLocation() {
 
     if (timer_is_on==1) {
     
-        var lati = 40.3 + randX + (randA * counter) - (Math.random()/200);
-        var longi = -74.5 + randY + (randB * counter) - (Math.random()/200);
+        var lati = 40.3 + randX + (randA * counter) - (Math.random()/90000); // random variation
+        var longi = -74.5 + randY + (randB * counter) - (Math.random()/90000);
         // -74.0, 40.7 NYC
-
-        dbWrite(rideID,counter,lati,longi);
-        cartodbTrace(rideID,counter,lati,longi);
-        openthedata(counter,lati,longi);
 
         // grab location to calc distance
         if ( counter == 0 ) { 
             startLat = lati;
             startLong = longi;
+            distance = 0;
+            distRide = 0;
             // console.log(startLat,startLong);
             }
-        // rideDistance(startLat,startLong,lati,longi);
-    
+         else {
+             rideDistance(prevLat,prevLong,lati,longi);
+    		}
+
+
         // console.log("point #" + counter + " lati:" + lati + " longi:" + longi );
 
+        dbWrite(rideID,counter,lati,longi,distance,distRide);
+        cartodbTrace(rideID,counter,lati,longi);
+        openthedata(counter,lati,longi,distance);
+
         counter=counter+1;
+        prevLat = lati;
+        prevLong = longi;
         timer=setTimeout("fakeLocation()",gpsInterval);    
     }
     else { ; }
@@ -199,7 +244,10 @@ function bikeLocation() {
                 dbWrite(counter,lati,longi);
                 cartodbTrace(rideID,counter,lati,longi);
                 openthedata(counter,lati,longi);
-            
+
+                // could be used save data locally and then send when online:
+                // https://github.com/alexgibson/OfflineForm/blob/master/offlineData.js
+                
             };
             var geoFail = function() {
                 // write failure to cartoDB ??
@@ -215,11 +263,11 @@ function bikeLocation() {
 }
 
 
-
+// reveal data to the user
 function openthedata(counter,lati,longi) {
-    document.getElementById('background').innerHTML += "Point: " + counter + ". ";
-    document.getElementById('background').innerHTML += "Lat: " + lati + ". ";
-    document.getElementById('background').innerHTML += "Long: " + longi + ". ";
+    document.getElementById('opendata').innerHTML += "Point: " + counter + ". ";
+    document.getElementById('opendata').innerHTML += "Lat: " + lati + ". ";
+    document.getElementById('opendata').innerHTML += "Long: " + longi + ". ";
 }
 
 
@@ -326,18 +374,18 @@ function dbDrop() {
 
 function init_db() {
     db.transaction(function (tx) {
-        tx.executeSql('CREATE TABLE IF NOT EXISTS bikedb (dbkey INTEGER PRIMARY KEY, userid INTEGER, rideid INTEGER, count INTEGER, lati INTEGER, longi INTEGER)');  
+        tx.executeSql('CREATE TABLE IF NOT EXISTS bikedb (dbkey INTEGER PRIMARY KEY, userid INTEGER, rideid INTEGER, count INTEGER, lati INTEGER, longi INTEGER, distance INTEGER, distRide INTEGER)');  
         console.log("db init");
     });
 }
 
-function dbWrite(rideid,thecount,lati,longi) {
+function dbWrite(rideid,thecount,lati,longi,distance) {
     if (write_local_db) {
         //console.log("write to localDB");
         var userid = userID;
         db.transaction(function (tx) {
             //tx.executeSql('INSERT INTO bikedb (count, lati, longi) VALUES ("'+ counter + '", "'+ lati +'", "'+ longi +'")' );
-            tx.executeSql('INSERT INTO bikedb (userid, rideid, count, lati, longi) VALUES (?,?,?,?,?);',[userid,rideid,thecount,lati,longi] );
+            tx.executeSql('INSERT INTO bikedb (userid, rideid, count, lati, longi, distance, distRide) VALUES (?,?,?,?,?,?,?);',[userid,rideid,thecount,lati,longi,distance,distRide] );
         });
         dbStatus();
     }
@@ -362,36 +410,67 @@ function rideCheck() {
 
 /******************************* 
  * Distance
- * http://www.movable-type.co.uk/scripts/latlong.html
+ * calculate each interval & aggregate the ride
  */
 
 function rideDistance(lat1,lon1,lat2,lon2) {    
 
-
-
-    function CalcDistanceBetween(lat1, lon1, lat2, lon2) {
-        //Radius of the earth in:  1.609344 miles,  6371 km  | var R = (6371 / 1.609344);
-        var R = 3958.7558657440545; // Radius of earth in Miles 
-        var dLat = toRad(lat2-lat1);
-        var dLon = toRad(lon2-lon1); 
-        var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-                Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) * 
-                Math.sin(dLon/2) * Math.sin(dLon/2); 
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-        var d = R * c;
-        return d;
-    }
-
-    function toRad(Value) {
-        /** Converts numeric degrees to radians */
-        return Value * Math.PI / 180;
-    }   
-
-    //var R = 6371; // km
-    //var d = Math.acos(Math.sin(lat1)*Math.sin(lat2) + Math.cos(lat1)*Math.cos(lat2) * Math.cos(lon2-lon1)) * R;
-    console.log("distance: " + d);
-
+	unit = "K";	
+	
+	var radlat1 = Math.PI * lat1/180
+	var radlat2 = Math.PI * lat2/180
+	var radlon1 = Math.PI * lon1/180
+	var radlon2 = Math.PI * lon2/180
+	var theta = lon1-lon2
+	var radtheta = Math.PI * theta/180
+	var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+	dist = Math.acos(dist)
+	dist = dist * 180/Math.PI
+	dist = dist * 60 * 1.1515
+	if (unit=="K") { dist = dist * 1.609344 }
+	if (unit=="N") { dist = dist * 0.8684 }
+	
+	distance = Math.round(dist * 1000); // km to meters
+	distRide +=  distance; 
+	
+	document.getElementById('distance').innerHTML = displayDistance(distRide);
+            
 }
+
+
+// display meters as km 
+function displayDistance(distRide){
+    
+    var distString = distRide.toString(); 
+	var distKm;
+	var distM;
+	// console.log(distRide,distString,distString.length);
+
+    // extract km
+	if ( distString.length > 4 ) { // if 10000+ meters
+        distKm = distString.substr(-5,2); // get km
+	    }
+	else if ( distString.length > 3 ) { // if 1000+ meters
+        distKm = distString.substr(-4,1); // get km
+	    }
+	else { distKm = 0;}
+	
+    // extract m
+	if ( distString.length > 2 ) { // if 100+ meters
+        distM = distString.substr(-3,2); // get km decimal
+	    }
+	else if ( distString.length > 1 ) { // if 10+ meters
+        distM = "0" + distString.substr(-2,1); // get km decimal
+	    }
+	else { distM = "00";}
+	
+	// console.log(distKm,distM);
+
+	var output = distKm + "." + distM + " km";	
+	return output; 
+}
+
+
 
 /******************************* 
  * Elapsed Timer
@@ -423,3 +502,40 @@ function checkTimer(i) {
     }
     return i;
 }
+
+
+
+/******************************* 
+ * User Settings
+ */
+
+function saveSettings() {
+    localStorage.username = document.getElementById('username').value;
+    console.log("saved username: " + localStorage.username);
+    //localStorage.email = document.getElementById('email').value;
+    //console.log("saved email: " + localStorage.email);
+    
+    window.location.href = "index.html"
+    return false;
+}
+
+function loadSettings() {
+	if (!localStorage.username) {
+	    localStorage.username = "";
+	}
+    document.getElementById('username').value = localStorage.username;
+    document.getElementById('settings').style.display = "block";
+    
+}
+
+function deleteSettings() {
+    var del=confirm("Delete Username?\nYou will lose access to your past rides.");    
+    if (del==true) {
+        localStorage.username = "";
+        console.log("username removed");
+        loadSettings();        
+    }
+    return false;
+}
+
+
